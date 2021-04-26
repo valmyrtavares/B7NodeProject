76050537d627c16eeed60967edecbf5e19e327a0
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-04-26 19:59:07 -0300

    Entendendo o Node #3

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const router = express.Router()
+
+
+router.get("/",(req, res)=>{
+    res.send("Ola mundo")
+})
+
+
+
+const app = express();
+
+app.use('/', router);
+
+module.exports = app
+
+
