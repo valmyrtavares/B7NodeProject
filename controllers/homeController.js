cb1cb657685830e5fd95187fb26456f3dfa0c11d
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-04-28 23:41:18 -0300

    mostrando posts na home aula 3/7

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,19 +1,15 @@
+const mongoose = require('mongoose')
+const Post = mongoose.model("Post")
 
 
+exports.index = async (req, res)=>{
+    
+    let responseJson = {
+        posts:[]
 
-exports.index = (req, res)=>{
-    res.render('home',{
-        'nome':'Valmyr Tavares',
-        'idade':50,
-        userInfo:req.userInfo,
-        mostrar:false,
-         testando: '<h1>Valmyr</h1>',
-         testa: '<h2>Valmyr Tavares</h2>',
-        ingredientes:[
-        
-        ],
-        interesses:[
-            'node','js','html'
-        ], 
-    });
+    }
+    const posts = await Post.find();
+    responseJson.posts = posts
+    
+    res.render('home',responseJson );
 }
