4bab07c8d2000e6f2bd2090a79de5fa359229055
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-05-01 23:23:52 -0300

    Acrescentando class

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const mongoose = require('mongoose')
 const Post = mongoose.model("Post")
 
@@ -6,10 +7,20 @@ exports.index = async (req, res)=>{
     
     let responseJson = {
         posts:[],
-        tags:[]
-    }
+        tags:[],
+        tag:''
+    };
+   
+    responseJson.tag = req.query.t
 
     const tags = await Post.getTagsList();
+        for(let i in tags){
+            if(tags[i]._id == responseJson.tag){
+                tags[i].class = 'selected'
+            }
+        }
+
+
     responseJson.tags = tags;
     
 
