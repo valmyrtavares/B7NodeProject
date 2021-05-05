7cc79d630b4c6ac5bd710565e5e10b9b4825d84c
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-05-05 10:27:37 -0300

    Finda 4/14 ressalvas a {...helpers} em app.js

--- app.js
+++ app.js
@@ -40,9 +40,15 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next)=>{
-    res.locals.h = helpers;
+    res.locals.h = { ...helpers };
     res.locals.flashes = req.flash();
     res.locals.user = req.user   
+
+    if(req.isAuthenticated()) {
+        res.locals.h.menu = res.locals.h.menu.filter(i=>i.logged);
+    }else{
+        res.locals.h.menu = res.locals.h.menu.filter(i=> i.guest);
+    }
     next();
 })
 
