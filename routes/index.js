93b0febd5d86fb6518ae404a91759f871229fad7
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-05-05 09:57:42 -0300

    Foi adicionado o middleware que interepta e protege as rotas Fim da aula 4/13

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,33 @@ const express = require('express')
 const homeController = require('../controllers/homeController')
 const usersController = require("../controllers/userController")
 const postController = require('../controllers/postController.js')
+const imageMiddleware = require('../middlewares/imageMiddlewares');
+const authMiddleware = require('../middlewares/authMiddleware')
 
 const router = express.Router();
-const imageMiddleware = require('../middlewares/imageMiddlewares');
-const { route } = require('../app');
+//const { route } = require('../app');
 
 router.get("/" ,homeController.index)
 
 router.get('/users/login', usersController.login)
 router.post('/users/login', usersController.loginAction)
+router.get('/users/logout', usersController.logout)
 
 
 router.get('/users/register', usersController.register)
 router.post('/users/register', usersController.registerAction)
-router.get('/users/logout', usersController.logout)
 
-router.get('/post/add', postController.add)
+router.get('/post/add', authMiddleware.isLogged, postController.add)
 router.post('/post/add',
+    authMiddleware.isLogged,
     imageMiddleware.upload,
     imageMiddleware.resize,
     postController.addAction
 )
 
-router.get('/post/:slug/edit',postController.edit)
+router.get('/post/:slug/edit', authMiddleware.isLogged,postController.edit)
 router.post('/post/:slug/edit',
+    authMiddleware.isLogged,
     imageMiddleware.upload,
     imageMiddleware.resize,
     postController.editAction)
